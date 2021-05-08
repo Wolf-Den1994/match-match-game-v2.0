fb00fb1d1b398890c779f1521b5282ffcf19317f
Denis <wolf-den@tut.by>
2021-05-08 18:37:27 +0300

    feat: add route about

--- src/routes/about.ts
+++ src/routes/about.ts
@@ -1,3 +1,43 @@
+const image1 = require('../assets/image1.png');
+const image2 = require('../assets/image2.png');
+const image3 = require('../assets/image3.png');
+
 export const about = `
-  <h1>I am About Page.</h1>
+  <main class="main">
+    <h2 class="main-title">How to play?</h2>
+    <div class="main-top">
+      <div class="main-top-card">
+        <div class="card-one card-number">
+          <div class="card-number-num">1</div>
+        </div>
+        <p>Register new player in game</p>
+      </div>
+      <div class="main-top-block-image">
+        <img src="${image1}" alt="register">
+      </div>
+    </div>
+    <div class="main-middle">
+      <div class="main-middle-card">
+        <div class="card-two card-number">
+          <div class="card-number-num">2</div>
+        </div>
+        <p>Configure your game settings</p>
+      </div>
+      <div class="main-middle-block-image">
+        <img src="${image2}" alt="settings">
+      </div>
+    </div>
+    <div class="main-bottom">
+      <div class="main-bottom-card">
+        <div class="card-three card-number">
+          <div class="card-number-num">3</div>
+        </div>
+        <p>Start you new game! Remember card <br>
+          positions and match it before times up.</p>
+      </div>
+      <div class="main-bottom-block-image">
+        <img src="${image3}" alt="settings">
+      </div>
+    </div>
+  </main>
 `;
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -12,63 +12,48 @@ const routes: IRoutes = {
   '/settings': settings,
 };
 
-const ulNavbar = document.createElement('ul');
-ulNavbar.className = 'navbar-list';
-document.body.append(ulNavbar);
+const linkNavbarAbout = document.getElementById('about');
+const linkNavbarScore = document.getElementById('score');
+const linkNavbarSettings = document.getElementById('settings');
+const root = document.getElementById('root');
+const navbarLinks = document.querySelectorAll('.navbar-link');
 
-const liNavbarAbout = document.createElement('li');
-liNavbarAbout.className = 'navbar-item';
-liNavbarAbout.id = 'about';
-ulNavbar.append(liNavbarAbout);
-
-const liNavbarScore = document.createElement('li');
-liNavbarScore.className = 'navbar-item';
-liNavbarScore.id = 'score';
-ulNavbar.append(liNavbarScore);
-
-const liNavbarSettings = document.createElement('li');
-liNavbarSettings.className = 'navbar-item';
-liNavbarSettings.id = 'settings';
-ulNavbar.append(liNavbarSettings);
-
-const linkNavbarAbout = document.createElement('a');
-linkNavbarAbout.href = '#';
-linkNavbarAbout.innerHTML = `About`;
-liNavbarAbout.append(linkNavbarAbout);
-
-const linkNavbarScore = document.createElement('a');
-linkNavbarScore.href = '#';
-linkNavbarScore.innerHTML = `Score`;
-liNavbarScore.append(linkNavbarScore);
-
-const linkNavbarSettings = document.createElement('a');
-linkNavbarSettings.href = '#';
-linkNavbarSettings.innerHTML = `Settings`;
-liNavbarSettings.append(linkNavbarSettings);
-
-const root = document.createElement('div');
-root.id = 'root';
-document.body.append(root);
-root.innerHTML = routes[window.location.pathname];
+if (root) root.innerHTML = routes[window.location.pathname];
+if (linkNavbarAbout) linkNavbarAbout.classList.add('active');
 
 const onNavigate = (pathname: string) => {
   window.history.pushState({}, pathname, window.location.origin + pathname);
-  root.innerHTML = routes[pathname];
+  if (root) root.innerHTML = routes[pathname];
 };
 
-linkNavbarAbout.onclick = () => {
-  onNavigate('/');
-  return false;
-};
-linkNavbarScore.onclick = () => {
-  onNavigate('/score');
-  return false;
-};
-linkNavbarSettings.onclick = () => {
-  onNavigate('/settings');
-  return false;
-};
+if (linkNavbarAbout)
+  linkNavbarAbout.onclick = () => {
+    navbarLinks.forEach((link) => {
+      link.className = 'navbar-link';
+    });
+    linkNavbarAbout.classList.add('active');
+    onNavigate('/');
+    return false;
+  };
+if (linkNavbarScore)
+  linkNavbarScore.onclick = () => {
+    navbarLinks.forEach((link) => {
+      link.className = 'navbar-link';
+    });
+    linkNavbarScore.classList.add('active');
+    onNavigate('/score');
+    return false;
+  };
+if (linkNavbarSettings)
+  linkNavbarSettings.onclick = () => {
+    navbarLinks.forEach((link) => {
+      link.className = 'navbar-link';
+    });
+    linkNavbarSettings.classList.add('active');
+    onNavigate('/settings');
+    return false;
+  };
 
 window.onpopstate = () => {
-  root.innerHTML = routes[window.location.pathname];
+  if (root) root.innerHTML = routes[window.location.pathname];
 };
