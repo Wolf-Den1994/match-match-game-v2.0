4ac1b0f176c94ae635fc365c1e3ea64232f9610b
Denis <wolf-den@tut.by>
2021-05-09 12:57:33 +0300

    feat: add route score

--- src/routes/score.ts
+++ src/routes/score.ts
@@ -1,3 +1,21 @@
+const avatar1 = require('../assets/avatar1.png');
+
 export const score = `
-  <h1>I am Score Page.</h1>
+  <main class="main">
+    <div class="main-title">Best players</div>
+    <div class="player">
+      <div class="player-info">
+        <div class="player-avatar">
+          <img src="${avatar1}" alt="avatar">
+        </div>
+        <div class="player-description">
+          <p class="player-name">Nicci Troiani</p>
+          <p class="player-email">nicci@gmail.com</p>
+        </div>
+      </div>
+      <div class="player-score">
+        <span>Score: </span><span id="score-l">465</span>
+      </div>
+    </div>
+  </main>
 `;
