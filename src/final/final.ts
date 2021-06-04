1b13f83e574a2afd0f8cb59440a29315f2d60c85
Denis <wolf-den@tut.by>
2021-06-04 09:53:19 +0300

    fix: returned checks elems from final and create function isnotfound

--- src/final/final.ts
+++ src/final/final.ts
@@ -2,16 +2,17 @@ import { getObjPeriod } from '../game/field-time';
 import { toScore } from '../score/to-score';
 import { addClassList } from '../utils/add-class';
 import { OPEN_MODAL } from '../utils/consts';
+import { isNotFound } from '../utils/errors';
 
 export const congratulateTheWinner = function congratulateWinner(): void {
-  const modalCongratulate = <HTMLElement>(
-    document.querySelector('.modal-congratulate')
-  );
-  const timeFromSpan = <HTMLElement>(
-    document.querySelector('.congratulate-time')
-  );
-  const overlay = <HTMLElement>document.getElementById('overlay');
+  const modalCongratulate = document.querySelector('.modal-congratulate');
+  const timeFromSpan = document.querySelector('.congratulate-time');
+  const overlay = document.getElementById('overlay');
   const time = getObjPeriod();
+
+  if (!modalCongratulate) throw new Error(`modal final ${isNotFound()}`);
+  if (!timeFromSpan) throw new Error(`span from time ${isNotFound()}`);
+  if (!overlay) throw new Error(`overlay ${isNotFound()}`);
 
   const h = time.hour.innerText;
   const m = time.min.innerText;
--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,6 +1,10 @@
+export const isNotFound = function isNotFoundElement(): string {
+  return 'is not found';
+};
+
 export const isInputElement = function inputElement(
   elem: HTMLElement | null,
 ): elem is HTMLInputElement {
-  if (!elem) throw new Error('Element is not found');
+  if (!elem) throw new Error(`Element ${isNotFound()}`);
   return elem.tagName === 'INPUT';
 };
